8456d5003ddadc007c3a770b5fc1482da3c260c3
Nuri <72916917+npuigdemont@users.noreply.github.com>
2021-12-10 13:37:54 +0100

    master

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = ''; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
